97bf152e45f56a1555b60d84257e073d1b1c5959
mrange <marten_range@hotmail.com>
2022-11-07 08:20:28 +0100

    Added empty statiq site

---
+++ src/Statiq.Site/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Statiq.Site
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var ci = CultureInfo.InvariantCulture;
+            CultureInfo.CurrentCulture                = ci;
+            CultureInfo.CurrentUICulture              = ci;
+            CultureInfo.DefaultThreadCurrentCulture   = ci;
+            CultureInfo.DefaultThreadCurrentUICulture = ci;
+
+            return await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync()
+                ;
+        }
+    }
+}
