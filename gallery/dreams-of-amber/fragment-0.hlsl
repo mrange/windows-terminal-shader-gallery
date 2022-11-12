7a835dfe631965886b1c3248b430825141845032
mrange <marten_range@hotmail.com>
2022-11-12 20:11:02 +0100

    Added 'dreams of amber'

---
+++ gallery/dreams-of-amber/fragment-0.hlsl
@@ -0,0 +1,257 @@
+#define WINDOWS_TERMINAL
+
+Texture2D shaderTexture;
+SamplerState samplerState;
+
+// --------------------
+#if defined(WINDOWS_TERMINAL)
+cbuffer PixelShaderSettings {
+  float  Time;
+  float  Scale;
+  float2 Resolution;
+  float4 Background;
+};
+
+#define TIME        Time
+#define RESOLUTION  Resolution
+#else
+float time;
+float2 resolution;
+
+#define TIME        time
+#define RESOLUTION  resolution
+#endif
+// --------------------
+
+// --------------------
+// GLSL => HLSL adapters
+#define vec2  float2
+#define vec3  float3
+#define vec4  float4
+#define mat2  float2x2
+#define mat3  float3x3
+#define fract frac
+#define mix   lerp
+
+float mod(float x, float y) {
+  return x - y * floor(x/y);
+}
+
+vec2 mod(vec2 x, vec2 y) {
+  return x - y * floor(x/y);
+}
+
+static const vec2 unit2 = vec2(1.0, 1.0);
+static const vec3 unit3 = vec3(1.0, 1.0, 1.0);
+static const vec4 unit4 = vec4(1.0, 1.0, 1.0, 1.0);
+
+// --------------------
+
+#define PI          3.141592654
+#define TAU         (2.0*PI)
+#define ROT(a)      mat2(cos(a), sin(a), -sin(a), cos(a))
+
+// License: WTFPL, author: sam hocevar, found: https://stackoverflow.com/a/17897228/418488
+static const vec4 hsv2rgb_K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+vec3 hsv2rgb(vec3 c) {
+  vec3 p = abs(fract(c.xxx + hsv2rgb_K.xyz) * 6.0 - hsv2rgb_K.www);
+  return c.z * mix(hsv2rgb_K.xxx, clamp(p - hsv2rgb_K.xxx, 0.0, 1.0), c.y);
+}
+// License: WTFPL, author: sam hocevar, found: https://stackoverflow.com/a/17897228/418488
+//  Macro version of above to enable compile-time constants
+#define HSV2RGB(c)  (c.z * mix(hsv2rgb_K.xxx, clamp(abs(fract(c.xxx + hsv2rgb_K.xyz) * 6.0 - hsv2rgb_K.www) - hsv2rgb_K.xxx, 0.0, 1.0), c.y))
+
+// License: Unknown, author: nmz (twitter: @stormoid), found: https://www.shadertoy.com/view/NdfyRM
+vec3 sRGB(vec3 t) {
+  return mix(1.055*pow(t, unit3*(1./2.4)) - 0.055, 12.92*t, step(t, unit3*(0.0031308)));
+}
+
+// License: Unknown, author: Matt Taylor (https://github.com/64), found: https://64.github.io/tonemapping/
+vec3 aces_approx(vec3 v) {
+  v = max(v, 0.0);
+  v *= 0.6f;
+  float a = 2.51f;
+  float b = 0.03f;
+  float c = 2.43f;
+  float d = 0.59f;
+  float e = 0.14f;
+  return clamp((v*(a*v+b))/(v*(c*v+d)+e), 0.0f, 1.0f);
+}
+
+// -------------------------------------------------
+// Mandelbox - https://www.shadertoy.com/view/XdlSD4
+static const float fixed_radius2 = 1.9;
+static const float min_radius2   = 0.5;
+static const float folding_limit = 1.0;
+static const float scale         = -2.8;
+// License: MIT, author: Inigo Quilez, found: https://www.iquilezles.org/www/articles/smin/smin.htm
+float pmin(float a, float b, float k) {
+  float h = clamp(0.5+0.5*(b-a)/k, 0.0, 1.0);
+  return mix(b, a, h) - k*h*(1.0-h);
+}
+
+// License: CC0, author: Mårten Rånge, found: https://github.com/mrange/glsl-snippets
+float pmax(float a, float b, float k) {
+  return -pmin(-a, -b, k);
+}
+
+// License: CC0, author: Mårten Rånge, found: https://github.com/mrange/glsl-snippets
+float pabs(float a, float k) {
+  return pmax(a, -a, k);
+}
+vec3 pmin(vec3 a, vec3 b, vec3 k) {
+  vec3 h = clamp( 0.5+0.5*(b-a)/k, 0.0, 1.0);
+
+  return mix(b, a, h) - k*h*(1.0-h);
+}
+void sphere_fold(inout vec3 z, inout float dz) {
+    float r2 = dot(z, z);
+    if(r2 < min_radius2) {
+        float temp = (fixed_radius2 / min_radius2);
+        z *= temp;
+        dz *= temp;
+    } else if(r2 < fixed_radius2) {
+        float temp = (fixed_radius2 / r2);
+        z *= temp;
+        dz *= temp;
+    }
+}
+
+void box_fold(inout vec3 z, inout float dz) {
+  const float k = 0.05;
+  // Soft clamp after suggestion from ollij
+  vec3 zz = sign(z)*pmin(abs(z), unit3*(folding_limit), unit3*(k));
+  // Hard clamp
+  // z = clamp(z, -folding_limit, folding_limit);
+  z = zz * 2.0 - z;
+}
+
+float sphere(vec3 p, float t) {
+  return length(p)-t;
+}
+
+float torus(vec3 p, vec2 t) {
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
+}
+
+// Adapted from Evilryu's: https://www.shadertoy.com/view/XdlSD4
+float mb(vec3 z) {
+    vec3 offset = z;
+    float dr = 1.0;
+    float fd = 0.0;
+    for(int n = 0; n < 5; ++n) {
+        box_fold(z, dr);
+        sphere_fold(z, dr);
+        z = scale * z + offset;
+        dr = dr * abs(scale) + 1.0;
+        float r1 = sphere(z, 5.0);
+        float r2 = torus(z, vec2(8.0, 1));
+        r2 = abs(r2) - 0.25;
+        float r = n < 4 ? r2 : r1;
+        float dd = r / abs(dr);
+        if (n < 3 || dd < fd) {
+          fd = dd;
+        }
+    }
+    return (fd);
+}
+
+vec2 toPolar(vec2 p) {
+  return vec2(length(p), atan2(p.y, p.x));
+}
+
+vec2 toRect(vec2 p) {
+  return vec2(p.x*cos(p.y), p.x*sin(p.y));
+}
+
+float modMirror1(inout float p, float size) {
+  float halfsize = size*0.5;
+  float c = floor((p + halfsize)/size);
+  p = mod(p + halfsize,size) - halfsize;
+  p *= mod(c, 2.0)*2.0 - 1.0;
+  return c;
+}
+// License: CC0, author: Mårten Rånge, found: https://github.com/mrange/glsl-snippets
+float smoothKaleidoscope(inout vec2 p, float sm, float rep) {
+  vec2 hp = p;
+
+  vec2 hpp = toPolar(hp);
+  float rn = modMirror1(hpp.y, TAU/rep);
+
+  float sa = PI/rep - pabs(PI/rep - abs(hpp.y), sm);
+  hpp.y = sign(hpp.y)*(sa);
+
+  hp = toRect(hpp);
+
+  p = hp;
+
+  return rn;
+}
+
+float df(vec2 p) {
+  const float s = 0.45;
+  p /= s;
+  float rep = 20.0;
+  float ss = 0.05*6.0/rep;
+  vec3 p3 = vec3(p.x, p.y, smoothstep(-0.9, 0.9, sin(TIME*0.21)));
+  p3.yz = mul(ROT(TIME*0.05), p3.yz);
+  p3.xy = mul(ROT(TIME*0.11), p3.xy);
+  float n = smoothKaleidoscope(p3.xy, ss, rep);
+  float d = mb(p3)*s;
+  return abs(d);
+}
+
+
+// -------------------------------------------------
+
+vec3 effect(vec2 p, vec2 pp) {
+  float aa = 2.0/RESOLUTION.y;
+
+  float d = df(p);
+  const float hoff = 0.67;
+  const float inte = 0.75;
+  const vec3 bcol0 = HSV2RGB(vec3(0.50+hoff, 0.85, inte*0.85));
+  const vec3 bcol1 = HSV2RGB(vec3(0.33+hoff, 0.85, inte*0.025));
+  vec3 col = 0.1*bcol0;
+  col += bcol1/sqrt(abs(d));
+  col += bcol0*smoothstep(aa, -aa, (d-0.001));
+
+  col *= smoothstep(1.5, 0.5, length(pp));
+
+  return col;
+}
+
+//
+// PS_OUTPUT ps_main(in PS_INPUT In)
+#if defined(WINDOWS_TERMINAL)
+float4 main(float4 pos : SV_POSITION, float2 tex : TEXCOORD) : SV_TARGET
+#else
+float4 ps_main(float4 pos : SV_POSITION, float2 tex : TEXCOORD) : SV_TARGET
+#endif
+{
+  vec2 q = tex;
+  vec2 p = -1.0 + 2.0*q;
+  vec2 pp = p;
+#if defined(WINDOWS_TERMINAL)
+  p.y = -p.y;
+#endif
+  p.x *= RESOLUTION.x/RESOLUTION.y;
+
+  vec3 col = effect(p, pp);
+  col *= smoothstep(1.5, 0.5, length(pp));
+  col = aces_approx(col);
+  col = sRGB(col);
+
+  vec4 fg = shaderTexture.Sample(samplerState, q);
+  vec4 sh = shaderTexture.Sample(samplerState, q-2.0*unit2/RESOLUTION.xy);
+
+  float ff = smoothstep(2.0, 0.5, length(pp));
+  col = mix(col, 0.0*unit3, sh.w*ff);
+  col = mix(col, fg.xyz, fg.w*ff);
+
+  return vec4(col, 1.0);
+}
+
+
+
